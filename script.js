5f85070e2edcff7f7811adb15d068a5b8e730227
Grant Deker (WSL2) <117955357+Kiddeke@users.noreply.github.com>
2024-02-14 21:23:29 -0500

    Practice more dom manipulation

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 
-const paragraph = document.createElement('p');
-paragraph.style.color = "red";
-paragraph.textContent = "Hey I'm red";
+// const paragraph = document.createElement('p');
+// paragraph.style.color = "red";
+// paragraph.textContent = "Hey I'm red";
 
-body.appendChild(paragraph);
+// body.appendChild(paragraph);
 
-const h3 = document.createElement('h3');
-h3.style.color = "blue";
-h3.textContent = "I'm a blue h3!";
+// const h3 = document.createElement('h3');
+// h3.style.color = "blue";
+// h3.textContent = "I'm a blue h3!";
 
-body.appendChild(h3);
+// body.appendChild(h3);
 
-const div = document.createElement('div');
-div.style.border = "black";
-div.style.background = "pink";
+// const div = document.createElement('div');
+// div.style.border = "black";
+// div.style.background = "pink";
 
-body.appendChild(div);
+// body.appendChild(div);
 
-const h1 = document.createElement('h3');
-h1.textContent = "I'm in a div";
+// const h1 = document.createElement('h3');
+// h1.textContent = "I'm in a div";
 
-div.appendChild(h1);
+// div.appendChild(h1);
 
-const p2 = document.createElement('h3');
-p2.textContent = "Me too!";
+// const p2 = document.createElement('h3');
+// p2.textContent = "Me too!";
 
-div.appendChild(p2);
+// div.appendChild(p2);
 
-btn.addEventListener('click', function (e) {
-    console.log(e);
-  });
+// const btn = document.querySelector('#btn');
+// btn.onclick = () => alert("Hello World");
+
+
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
+
+// btn.addEventListener('click', function (e) {
+//     console.log(e.div);
+//   });
