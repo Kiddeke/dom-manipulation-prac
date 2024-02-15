90f4f6c5fa594e8f989a7c204e8297ab3ebb8cc3
Grant Deker (WSL2) <117955357+Kiddeke@users.noreply.github.com>
2024-02-14 22:08:04 -0500

    Create a new paragraph using Document.createElement() and give it some text content in the same way as before

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ link.textContent = "Mozilla Developer Network";
 link.href = "https://developer.mozilla.org";
 
 const sect = document.querySelector("section");
+
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride.";
